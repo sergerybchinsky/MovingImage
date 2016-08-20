b19a6c1b95956c3a705d1e18aeed1d8b43927473
SergeRybchinsky <SergeRybchinsky@gmail.com>
2016-08-20 12:39:51 +0700

    Created ios proj.

---
+++ MovingImage/MovingImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovingImage
+//
+//  Created by SergeRybchinsky on 18/08/16.
+//  Copyright © 2016 SergeRybchinsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
